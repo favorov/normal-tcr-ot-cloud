8776625247b063dabeb308f669aed39b78275475
Alexander Favorov <favorov@sensi.org>
2026-02-13 04:23:22 -0500

    added some olga results from KarchinLab/TCRtoolkit nextflow pipeline example

---
+++ archive/copy_olga_tcr_pgen_from_nextflow.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mkdir -p ../samples_to_map/
+grep OLGA_PGEN .nextflow.log | grep "status: COMPLETED;" | grep _Post | cut -d " " -f 15,23 | sed "s/]//; s/_Post);//; s/(//" | awk '{print "cp "$2"/"$1"_Post_tcr_pgen.tsv ../samples_to_map/"}' | bash
