1270589e78b71cb4667d83773f8dc7074999c310
Alexander Favorov <favorov@sensi.org>
2026-02-14 01:07:44 -0500

    now, just find -exec cp

--- archive/copy_olga_tcr_pgen_from_nextflow.sh
+++ archive/copy_olga_tcr_pgen_from_nextflow.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ../samples_to_map/
-grep OLGA_PGEN .nextflow.log | grep "status: COMPLETED;" | grep _Post | cut -d " " -f 15,23 | sed "s/]//; s/_Post);//; s/(//" | awk '{print "cp "$2"/"$1"_Post_tcr_pgen.tsv ../samples_to_map/"}' | bash
+#grep OLGA_PGEN .nextflow.log | grep "status: COMPLETED;" | grep _Post | cut -d " " -f 15,23 | sed "s/]//; s/_Post);//; s/(//" | awk '{print "cp "$2"/"$1"_Post_tcr_pgen.tsv ../samples_to_map/"}' | bash
+find work/ -name "*pgen*" -and -name "*tsv" -and -name "Patient*" -exec cp {} ../samples_to_map/ \;
+
+
