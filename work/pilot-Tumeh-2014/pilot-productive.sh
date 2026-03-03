30978664038f6504fc5d493c06b7ab605989f232
Sasha Favorov <favorov@sensi.org>
2026-03-03 16:23:41 -0500

    pilot run, ok

--- work/pilot-Tumeh-2014/pilot-productive.sh
+++ work/pilot-Tumeh-2014/pilot-productive.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-
-../olga-p2b-p2b-mds-plot-samples-and-bc.py  pilot-cloud-Tumeh2014-Base/ pilot-mapped-Tumeh2014-Post/ --productive-filter
-../olga-samples-p2b-pval.py  pilot-cloud-Tumeh2014-Base/ pilot-mapped-Tumeh2014-Post/ --productive-filter > pilot-productive-filter-results/post-p-values.txt
+../../olga-p2b-ot.py  cloud-Tumeh2014-Base/ \
+  mapped-Tumeh2014-Post/ --productive-filter > \
+  pilot-productive-filter-results/distances.txt
+../../olga-samples-p2b-pval.py cloud-Tumeh2014-Base/ \
+  mapped-Tumeh2014-Post/ --productive-filter > \
+  pilot-productive-filter-results/post-p-values.txt
+../../olga-boxplot-samples-ot-2pb.py cloud-Tumeh2014-Base/ \
+  mapped-Tumeh2014-Post/ --productive-filter \
+  --output-plot pilot-productive-filter-results/boxplot.png
+../../olga-p2b-mds-plot-samples-and-bc.py  cloud-Tumeh2014-Base/ \
+  mapped-Tumeh2014-Post/ --productive-filter \
+  --output-plot pilot-productive-filter-results/mds.png
