552a4525bc96485b451f6b397a21db643e4688ce
Sasha Favorov <favorov@sensi.org>
2026-03-03 12:14:32 -0500

    renaming files and foldrs for pilot

--- work/pilot-Tumeh-2014/pilot-productive.sh
+++ work/pilot-Tumeh-2014/pilot-productive.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 ../olga-p2b-p2b-mds-plot-samples-and-bc.py  pilot-cloud-Tumeh2014-Base/ pilot-mapped-Tumeh2014-Post/ --productive-filter
 ../olga-samples-p2b-pval.py  pilot-cloud-Tumeh2014-Base/ pilot-mapped-Tumeh2014-Post/ --productive-filter > pilot-productive-filter-results/post-p-values.txt
